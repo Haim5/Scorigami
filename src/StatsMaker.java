cdd682a6d311830e6794dcff63f91ac146c7ecaf
Haim5 <89258839+Haim5@users.noreply.github.com>
2022-01-27 01:02:07 +0200

    Update StatsMaker.java

--- src/StatsMaker.java
+++ src/StatsMaker.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,6 +9,11 @@ import java.util.List;
 public class StatsMaker {
     private final ScorigamiMap sm;
     private int games;
+    private int diffScores;
+    // Possession Margin List. PL[i] = minimal number of possessions needed to gain i points.
+    private final ArrayList<Integer> PML;
+    // FG: field goal (3 points), UTRY: unconverted try (5 points), CTRY: converted try (7 points).
+    private final static int FG = 3, UTRY = 5, CTRY = 7;
 
     /**
      * Constructor.
@@ -15,7 +21,10 @@ public class StatsMaker {
      */
     StatsMaker(ScorigamiMap sm) {
         this.sm = sm;
-        this.numberOfGames();
+        this.games = this.numberOfGames();
+        this.diffScores = numberOfScoresHelper();
+        // initialise base values [0-7]
+        PML = new ArrayList<>(Arrays.asList(0, Integer.MAX_VALUE, Integer.MAX_VALUE, 1, Integer.MAX_VALUE, 1, 2, 1));
     }
 
     /**
@@ -38,14 +47,16 @@ public class StatsMaker {
         if (isScorigami(s)) {
             return s;
         }
+        // set minimalMargin to max value.
+        int minimalMargin = Integer.MAX_VALUE;
+        // initialise constant variables.
+        final int home = s.getHomeScore(), away = s.getAwayScore(), sum = home + away;
         Score answer = null;
-        int minimalMargin = Integer.MAX_VALUE, home = s.getHomeScore(), away = s.getAwayScore(), h, i, j, k;
-        int l = home + away;
-        for (i = away, h = 0; h <= minimalMargin; i++, h++) {
-            for (j = home, k = h; k <= minimalMargin; j++, k++) {
+        for (int i = away, h = 0; h <= minimalMargin; i++, h++) {
+            for (int j = home, k = h; k <= minimalMargin; j++, k++) {
                 Score temp = new Score(j, i);
-                int currMargin = i + j - l;
-                if (currMargin < minimalMargin && isScorigami(temp) && isValidRugbyDistance(s, temp)) {
+                int currMargin = i + j - sum;
+                if (currMargin < minimalMargin && s.isValidDistance(temp) && isScorigami(temp)) {
                     answer = temp;
                     minimalMargin = currMargin;
                 }
@@ -54,18 +65,6 @@ public class StatsMaker {
         return answer;
     }
 
-    /**
-     * check if the margin between the two scores is valid in rugby.
-     * @param og the original score.
-     * @param target the target score.
-     * @return if the margin is valid - true, else - false.
-     */
-    private boolean isValidRugbyDistance(Score og, Score target) {
-        int homeMargin = Math.abs(og.getHomeScore() - target.getHomeScore());
-        int awayMargin = Math.abs(og.getAwayScore() - target.getAwayScore());
-        return  !(homeMargin == 1 || homeMargin == 2 || homeMargin == 4 ||
-                awayMargin == 1 || awayMargin == 2 || awayMargin == 4);
-    }
 
     /**
      * get the closest scorigami score, closest by possessions.
@@ -77,23 +76,36 @@ public class StatsMaker {
         if (isScorigami(s)) {
             return s;
         }
+        // initialise minimalPoss, minimalMargin and maxMargin to the max value.
         int minimalPoss = Integer.MAX_VALUE, minimalMargin = Integer.MAX_VALUE, maxMargin = Integer.MAX_VALUE;
-        int home = s.getHomeScore(), away = s.getAwayScore(), h, i ,j, k, l = home + away;
+        // initialise constant variables.
+        final int home = s.getHomeScore(), away = s.getAwayScore(), sum = home + away;
         Score answer = null;
-        for (i = away, h = 0; h <= maxMargin; i++, h++) {
-            for (j = home, k = h; k <= maxMargin; j++, k++) {
+        // i - away team score, h - margin from original away score (i - away).
+        for (int i = away, h = 0; h <= maxMargin; i++, h++) {
+            // check if h is a valid margin by comparing (h:0) with (0:0).
+            if (!s.isValidDistance(new Score(home, i))) {
+                continue;
+            }
+            int awayPM = PML.get(h);
+            // j - home team score, k - total margin (i + j - home - away).
+            for (int j = home, k = h; k <= maxMargin; j++, k++) {
                 Score temp = new Score(j, i);
-                if (isValidRugbyDistance(s, temp) && isScorigami(temp)) {
-                    int currentPossMargin = s.distanceByPoss(temp);
-                    int currentPointMargin = i + j - l;
-                    if (currentPossMargin == minimalPoss && currentPointMargin < minimalMargin) {
-                        answer = temp;
-                        minimalMargin = currentPointMargin;
-                    } else if (currentPossMargin < minimalPoss) {
+                // update PML.
+                possessionMarginListUpdate(k - h);
+                // check score validity and if the score is a scorigami.
+                if (s.isValidDistance(temp) && isScorigami(temp)) {
+                    // set values
+                    int currentPossMargin = awayPM + PML.get(k - h);
+                    int currentPointMargin = i + j - sum;
+                    if (currentPossMargin < minimalPoss) {
                         answer = temp;
                         minimalMargin = currentPointMargin;
                         minimalPoss = currentPossMargin;
-                        maxMargin = 7 * minimalPoss;
+                        maxMargin = CTRY * minimalPoss;
+                    } else if (currentPossMargin == minimalPoss && currentPointMargin < minimalMargin) {
+                        answer = temp;
+                        minimalMargin = currentPointMargin;
                     }
                 }
             }
@@ -102,28 +114,15 @@ public class StatsMaker {
     }
 
     /**
-     * get a list of the most common scores.
-     * @return a list of the most common scores.
+     * update PML if needed.
+     * @param m the current index.
      */
-    private LinkedList<Score> getCommonList() {
-        int max = 0;
-        LinkedList<Score> commonList = new LinkedList<>();
-        LinkedList<Score> keyList = (LinkedList<Score>) this.sm.getScoreList();
-        for (Score s : keyList) {
-            MatchList ml = this.sm.getMatchListByScore(s);
-            int times = 0;
-            if (ml != null) {
-                times = ml.getSize();
-            }
-            if (times > max) {
-                commonList.clear();
-                commonList.add(s.getWinningFirst());
-                max = times;
-            } else if (times == max) {
-                commonList.add(s.getWinningFirst());
-            }
+    private void possessionMarginListUpdate(int m) {
+        // if index m is not in the array, add it.
+        if (m >= PML.size()) {
+            // dynamic programming - get the distance using previous solutions.
+            PML.add(m, 1 + Math.min(Math.min(PML.get(m - FG), PML.get(m - UTRY)), PML.get(m - CTRY)));
         }
-        return commonList;
     }
 
     /**
@@ -135,16 +134,20 @@ public class StatsMaker {
     }
 
     /**
-     * add 1 to the number of games.
+     * add 1 to the number of games and to diffScores if needed.
+     * @param isScorigami is the score a scorigami.
      */
-    public void add() {
+    public void add(boolean isScorigami) {
         this.games += 1;
+        if (isScorigami) {
+            this.diffScores += 1;
+        }
     }
 
     /**
      * set this.games field using the database.
      */
-    private void numberOfGames() {
+    private int numberOfGames() {
         List<Score> keyList = this.sm.getScoreList();
         int count = 0;
         for (Score s : keyList) {
@@ -153,7 +156,7 @@ public class StatsMaker {
                 count += ml.getSize();
             }
         }
-        this.games = count;
+        return count;
     }
 
     /**
@@ -185,19 +188,14 @@ public class StatsMaker {
      * @return number of unique scores.
      */
     public int numberOfScores() {
-        return this.sm.getScoreSet().size();
+        return diffScores;
     }
 
-     /**
-     * get the biggest number of points ever scored by one team in a game.
-     * @return biggest score.
+    /**
+     * helper method, gets the number of unique scores that have ever happened. used to reduce complexity.
+     * @return number of unique scores.
      */
-    public int mostPointsByTeam() {
-        List<Score> l = sm.getScoreList();
-        int max = 0;
-        for(Score s: l) {
-            max = Math.max(Math.max(s.getHomeScore(), s.getAwayScore()), max);
-        }
-        return max;
+    private int numberOfScoresHelper() {
+        return this.sm.getScoreSet().size();
     }
 }
