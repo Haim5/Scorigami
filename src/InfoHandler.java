9f197b7a45111221f07be31a42f5a745967721ce
Haim5 <89258839+Haim5@users.noreply.github.com>
2022-01-30 11:29:55 +0200

    Update InfoHandler.java

--- src/InfoHandler.java
+++ src/InfoHandler.java
@@ -2,14 +2,25 @@ abstract public class InfoHandler {
     protected int limit = Integer.MAX_VALUE;
     protected int minPts = Integer.MAX_VALUE;
     protected Score ans = null;
+    protected ScoreInfo curr = new ScoreInfo();
+    protected String description;
 
     /**
      * reset the values so we can use the handler again.
      */
-    public void reset() {
+    protected void reset() {
         limit = Integer.MAX_VALUE;
         minPts = Integer.MAX_VALUE;
         ans = null;
+        this.curr.reset();
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Score getCurrentScore() {
+        return curr.score;
     }
 
     /**
@@ -42,15 +53,13 @@ abstract public class InfoHandler {
 
     /**
      * change the values as needed.
-     * @param curr current data to compare.
      */
-    abstract void handle(ScoreInfo curr);
+    abstract void handle();
 
     /**
      * set the values of curr.
-     * @param curr scoreInfo obj we need to set its values.
      * @param og the original score.
      * @param temp the current score we compare with.
      */
-    abstract void setValues(ScoreInfo curr, Score og, Score temp);
+    abstract void setValues(Score og, Score temp);
 }
