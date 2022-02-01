783d3e8d7900077762151128c175b9723a6f9613
Haim5 <89258839+Haim5@users.noreply.github.com>
2022-02-01 11:37:39 +0200

    Update PtsScoreInfoHandler.java

--- src/PtsScoreInfoHandler.java
+++ src/PtsScoreInfoHandler.java
@@ -21,4 +21,9 @@ public class PtsScoreInfoHandler extends InfoHandler {
         this.curr.score = temp;
         this.curr.ptsMargin = og.distanceByPoints(temp);
     }
+
+    @Override
+    public boolean shouldContinue(int n) {
+        return (n == 1 || n == 2 || n == 4);
+    }
 }
