e34b6c48e1bc056d591ecf1de112f1844036e3fa
Haim5 <89258839+Haim5@users.noreply.github.com>
2022-03-28 11:12:31 +0300

    Update PossScoreInfoHandler.java

--- src/PossScoreInfoHandler.java
+++ src/PossScoreInfoHandler.java
@@ -1,12 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class PossScoreInfoHandler extends InfoHandler {
-    // constant variables.
-    private final static int FG = 3, UTRY = 5, CTRY = 7;
-    // Possession Margin List. PML[i] = minimal number of possessions needed to gain i points. [0-7] initialised.
-    private final ArrayList<Integer> PML = new ArrayList<>(Arrays.asList(0, Integer.MAX_VALUE, Integer.MAX_VALUE, 1, Integer.MAX_VALUE, 1, 2, 1));
-    private int minPoss = Integer.MAX_VALUE;
+    private final PML pml = new PML();
 
     /**
      * constructor, sets the description.
@@ -17,50 +10,23 @@ public class PossScoreInfoHandler extends InfoHandler {
 
     @Override
     public void handle() {
-        if (curr.possMargin < this.minPoss) {
-            this.ans = this.curr.score;
-            this.minPoss = this.curr.possMargin;
-            this.minPts = this.curr.ptsMargin;
-            this.limit = CTRY * this.curr.possMargin;
-        } else if (this.curr.possMargin == this.minPoss && this.curr.ptsMargin < this.minPts) {
-            this.ans = this.curr.score;
-            this.minPts = this.curr.ptsMargin;
+        if (curr.possMargin < answer.possMargin) {
+            this.answer = new ScoreInfo(this.curr);
+            this.limit = PML.CTRY * this.answer.possMargin;
+        } else if (curr.possMargin == answer.possMargin && curr.ptsMargin < answer.ptsMargin) {
+            this.answer.score = this.curr.score;
+            this.answer.ptsMargin = this.curr.ptsMargin;
         }
     }
 
     @Override
     public void setValues(Score temp, int homeMargin, int awayMargin) {
-        this.curr.score = temp;
-        this.curr.ptsMargin = homeMargin + awayMargin;
-        extendPML(Math.max(homeMargin, awayMargin));
-        this.curr.possMargin = PML.get(homeMargin) + PML.get(awayMargin);
-    }
-
-
-    @Override
-    protected void reset() {
-        super.reset();
-        this.minPoss = Integer.MAX_VALUE;
-    }
-
-    /**
-     * extend the PML if needed.
-     * @param index the index we need to add.
-     */
-    private void extendPML(int index) {
-        if (index < PML.size()) {
-            return;
-        }
-        for (int i = PML.size(); i <= index; i++) {
-            // calculate the new value using the previous results (dynamic programming).
-            PML.add(i, 1 + Math.min(Math.min(PML.get(i - FG), PML.get(i - UTRY)), PML.get(i - CTRY)));
-        }
+        this.curr.setValues(pml.get(homeMargin) + pml.get(awayMargin), homeMargin + awayMargin, temp);
     }
 
     @Override
     public boolean shouldContinue(int n) {
-        int v = PML.get(n);
-        return v == Integer.MAX_VALUE ||  v > this.minPoss;
+        int v = pml.get(n);
+        return (v == Integer.MAX_VALUE ||  v > this.answer.possMargin);
     }
 }
-
